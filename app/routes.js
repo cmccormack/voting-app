c8f1a309179c43eadcc19cdc3b9aa9983525232b
cmccormack <mccormack.christopher@gmail.com>
2017-11-14 21:25:43 -0700

    Added catch-all for non-react router routes

--- app/routes.js
+++ app/routes.js
@@ -18,12 +18,19 @@ module.exports = (app) => {
   app.post('/login', (req, res) => {
     res.type('json').send(JSON.stringify(
       {
-        'username': req.params.username,
-        'password': req.params.password
+        'username': req.body.username,
+        'password': req.body.password
       }
     ))
   })
 
+  // Handle invalid routes that React Router cannot
+  app.get('/:path/*', (req, res) => {
+    console.log(`New Request for ${req.hostname + req.path}`)
+    res.redirect('/')
+  })
+
+  // Default route loads React app
   app.get('*', (req, res) => {
     console.log(`New Request for ${req.hostname + req.path}`)
     res.sendFile(path.join(static, 'index.html'))

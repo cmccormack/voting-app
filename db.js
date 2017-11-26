756f7d620da5b618b1cc750df8876e6be845b1c2
Christopher McCormack <mccormack.christopher@gmail.com>
2017-11-26 12:42:47 -0700

    Moved db configs to seperate module

---
+++ db.js
@@ -0,0 +1,12 @@
+const path = require('path')
+require('dotenv').config({path: path.resolve(__dirname, '.env')})
+
+const { dbuser, dbpw, dbhost, dbname } = process.env
+module.exports = {
+  url: `mongodb://${dbhost}/${dbname}`,
+  options: {
+    useMongoClient: true,
+    user: dbuser,
+    pass: dbpw
+  }
+}

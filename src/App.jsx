c7b6eadab7ce03a8bc098c89a34dc58ed59a6699
Christopher McCormack <mccormack.christopher@gmail.com>
2017-12-04 11:38:04 -0700

    Added redirect after registration and converted functional component to class

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { 
+  BrowserRouter,
+  Redirect,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 import Main from './views/Main.jsx'
 import Login from './views/Login.jsx'
@@ -10,28 +16,49 @@ import './styles/body.scss'
 
 const entry_point = document.getElementById('root')
 
-const App = () => (
-  <div>
-    <nav style={{backgroundColor: 'lightcyan'}}>
-      <ul>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/main">Main</Link></li>
-      </ul>
-    </nav>
-    <div>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/main" component={Main} />
-        <Route exact path="/" component={Main} />
-        <Route render={(props) => (
-          <h1>404 Page not found</h1>
-        )}/>
-      </Switch>
-    </div>
-  </div>
-)
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      loggedIn: false
+    }
+    this.updateLoggedIn = this.updateLoggedIn.bind(this)
+  }
+
+  updateLoggedIn(loggedIn){
+    this.setState({ loggedIn })
+  }
+
+  render() {
+    return (
+      <div>
+        <nav style={{ backgroundColor: 'lightseagreen'}}>
+          <ul>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/main">Main</Link></li>
+          </ul>
+        </nav>
+        <div>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" render={() => (
+              this.state.loggedIn
+              ? ( <Redirect to='/main' /> )
+              : ( <Register loggedIn={this.updateLoggedIn} />)
+            )}/>
+            <Route exact path="/main" component={Main} />
+            <Route exact path="/" component={Main} />
+            <Route render={(props) => (
+              <h1>404 Page not found</h1>
+            )}/>
+          </Switch>
+        </div>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render((
     <BrowserRouter>

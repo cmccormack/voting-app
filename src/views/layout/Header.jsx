e998769bf9375324e7683cd2b1457a54ad6efae3
Christopher McCormack <mccormack.christopher@gmail.com>
2017-12-04 21:43:51 -0700

    Initial Commit

---
+++ src/views/layout/Header.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Redirect, Link } from 'react-router-dom'
+
+class Header extends Component {
+
+  constructor(props){
+    super(props)
+
+  }
+
+  render() {
+
+    console.log(this.props.loggedIn)
+
+    const links = (
+      <ul className="right hide-on-med-and-down">
+        <li><Link to="/main">Main</Link></li>
+        { 
+          this.props.loggedIn && 
+          <li><a onClick={this.props.handleLogout}>Logout</a></li>
+        } 
+        { !this.props.loggedIn && <li><Link to="/login">Login</Link></li> }
+        { !this.props.loggedIn && <li><Link to="/register">Register</Link></li> }
+      </ul>
+    )
+
+    return (
+      <nav className="row teal lighten-1">
+        <div className="nav-wrapper">
+          <div className="col s12">
+            <Link to='/' className="brand-logo">Votery</Link>
+            { links }
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Header

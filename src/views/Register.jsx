8ddd881e5617710b9914bf76b66b2f85756454c6
Christopher McCormack <mccormack.christopher@gmail.com>
2017-12-04 21:47:26 -0700

    Updated

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -8,8 +8,7 @@ class Register extends Component {
     super(props)
     this.state = {
       username: '',
-      password: '',
-      alert: { visible: false }
+      password: ''
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -25,28 +24,23 @@ class Register extends Component {
 
   handleSubmit(event) {
     event.preventDefault()
+
     const body = {
       username: this.state.username,
       password: this.state.password
     }
-
     const myHeaders = new Headers()
     myHeaders.append('Content-Type', 'application/json')
     fetch('/register', {
       method: 'POST',
       headers: myHeaders,
       cache: 'default',
+      credentials: 'same-origin',
       body: JSON.stringify(body)
     })
     .then(res => res.json()).then(data => {
       // Response from registration attempt
       console.log(data)
-      this.setState({alert: {
-        success: data.success ? 'alert-success' : 'alert-warning',
-        title: data.success ? 'Success!' : 'Warning!',
-        message: data.message,
-        visible: true
-      }})
       this.props.loggedIn(data.success)
     })
     .catch(err => { console.error(err) })
@@ -55,37 +49,46 @@ class Register extends Component {
   render() {
 
     return (
-      <div>
-        { this.state.alert.visible && <Alert {...this.state.alert} />}
-        <h1>Register</h1>
-        <form id='login_form' onSubmit={this.handleSubmit}>
-    
-          <label htmlFor='username'>Username: </label>
-          <input
-            id='input_username'
-            name='username'
-            onChange={this.handleInputChange}
-            placeholder='Username'
-            type='text'
-            value={this.state.username}
-          />
-          <br />
-    
-          <label htmlFor='password'>Password: </label>
-          <input
-            id='input_password'
-            name='password'
-            onChange={this.handleInputChange}
-            placeholder='Password'
-            required
-            type='password'
-            value={this.state.password}
-          />
-          <br />
-    
-          <button type='submit'>Submit</button>
-    
-        </form>
+      <div className="container">
+        <div className="row">
+          <div className="col s12">
+            <div className="card">
+              <div className="card-content">
+                <span className="card-title teal-text text-darken-1">
+                  {"Register"}
+                </span>
+                <form id='login_form' onSubmit={this.handleSubmit}>
+            
+                  <label htmlFor='username'>Username: </label>
+                  <input
+                    id='input_username'
+                    name='username'
+                    onChange={this.handleInputChange}
+                    placeholder='Username'
+                    type='text'
+                    value={this.state.username}
+                  />
+                  <br />
+            
+                  <label htmlFor='password'>Password: </label>
+                  <input
+                    id='input_password'
+                    name='password'
+                    onChange={this.handleInputChange}
+                    placeholder='Password'
+                    required
+                    type='password'
+                    value={this.state.password}
+                  />
+                  <br />
+            
+                  <button type='submit'>Submit</button>
+            
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }

38417f4b33363b996da96b0e1997fdc748fc4602
Christopher McCormack <mccormack.christopher@gmail.com>
2017-12-13 18:08:25 -0700

    Fixed sideNav

--- src/views/layout/Header.jsx
+++ src/views/layout/Header.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import { Redirect, Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 class Header extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
+  }
+
+  componentDidMount() {
+    $('.button-collapse').sideNav()
   }
 
   render() {
 
     const {loggedIn, user, handleLogout} = this.props
-    console.log(user)
 
     const links = (
       <ul className="right hide-on-med-and-down">
@@ -22,18 +26,38 @@ class Header extends Component {
       </ul>
     )
 
+    const sidenav = (
+      <ul className="side-nav" id="sideNav">
+        <li hidden={!loggedIn}><Link to="/user">{`Hello, ${user}!`}</Link></li>
+        <li><Link to="/main">Main</Link></li>
+        <li><div className="divider"></div></li>
+        <li hidden={loggedIn}><Link to="/login">Login</Link></li>
+        <li hidden={loggedIn}><Link to="/register">Register</Link></li>
+        <li hidden={!loggedIn}><a onClick={handleLogout}>Logout</a></li>
+      </ul>
+    )
+
     return (
-      <nav className="row teal lighten-1">
-        <div className="nav-wrapper">
-          <div className="col s12">
-            <Link to='/main' className="brand-logo">Votery</Link>
-            <div className="right">
-              { user && <span></span> }
-              { links }
+      <div>
+        { sidenav }
+        <div className="navbar-fixed row">
+          <nav className="teal lighten-1">
+            <div className="nav-wrapper">
+              <div className="col s12">
+                <Link to='/main' className="brand-logo">Votery</Link>
+                { links }
+                <a
+                  className="button-collapse right hide-on-large"
+                  href="#"
+                  data-activates="sideNav"
+                >
+                  <i className="material-icons">menu</i>
+                </a>
+              </div>
             </div>
-          </div>
+          </nav>
         </div>
-      </nav>
+      </div>
     )
   }
 }

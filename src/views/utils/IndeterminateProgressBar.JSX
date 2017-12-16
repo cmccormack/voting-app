a66dd698f64e288856390f3352f123ace4c21408
Christopher McCormack <mccormack.christopher@gmail.com>
2017-12-16 10:33:38 -0700

    Created progress bar and moved spinner

---
+++ src/views/utils/IndeterminateProgressBar.JSX
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function IndeterminateProgressBar () {
+  return (
+    <div className="progress">
+      <div className="indeterminate"></div>
+    </div>
+  )
+}
---
+++ src/views/utils/Spinner.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const SpinnerWrapper = ({size, children}) => (
+  <div className={`preloader-wrapper ${size} active`}>
+    { children }
+  </div>
+)
+
+const SpinnerBody = ({color}) => (
+  <div className={`spinner-layer spinner-${color}`}>
+    <div className="circle-clipper left">
+      <div className="circle"></div>
+    </div><div className="gap-patch">
+      <div className="circle"></div>
+    </div><div className="circle-clipper right">
+      <div className="circle"></div>
+    </div>
+  </div>
+)
+
+const Spinner = props => (
+  <SpinnerWrapper size="big">
+    <SpinnerBody color="blue" />
+    <SpinnerBody color="red" />
+    <SpinnerBody color="yellow" />
+    <SpinnerBody color="green" />
+  </SpinnerWrapper>
+
+)
+
+export default Spinner

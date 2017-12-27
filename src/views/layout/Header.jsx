4fd03f799a5ac8432289e028103f92dd5c2c6ab2
Christopher McCormack <mccormack.christopher@gmail.com>
2017-12-26 19:45:24 -0700

    Refactored

--- src/views/layout/Header.jsx
+++ src/views/layout/Header.jsx
@@ -2,6 +2,18 @@ import React, { Component } from 'react'
 import { Redirect, Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+const NavItem = ({ hidden, to, content }) => (
+  <li hidden={hidden}><Link to={to}>{content}</Link></li>
+)
+
+const Nav = ({ className, id, items }) => (
+  <ul className={className} id={id}>
+    {items.map((item, i) => (
+      <NavItem {...item} key={item.content} />
+    ))}
+  </ul>
+)
+
 class Header extends Component {
 
   constructor(props) {
@@ -16,7 +28,7 @@ class Header extends Component {
 
     const {loggedIn, user, handleLogout} = this.props
 
-    const links = (
+    const linksold = (
       <ul className="right hide-on-med-and-down">
         <li hidden={!loggedIn}><Link to="/user">{`Hello, ${user}!`}</Link></li>
         <li><Link to="/main">Main</Link></li>
@@ -25,6 +37,7 @@ class Header extends Component {
         <li hidden={!loggedIn}><Link to="/logout">Logout</Link></li>
       </ul>
     )
+
 
     const sidenav = (
       <ul className="side-nav" id="sideNav">
@@ -37,19 +50,34 @@ class Header extends Component {
       </ul>
     )
 
+
+
+
+    const links = [
+      { hidden: !loggedIn, to: '/user', content: `Hello, ${user}!` },
+      { hidden: false, to: '/main', content: 'Main' },
+      { hidden: loggedIn, to: '/login', content: 'Login' },
+      { hidden: loggedIn, to: '/register', content: 'Register' },
+      { hidden: !loggedIn, to: '/logout', content: 'Logout' }
+    ]
+
     return (
       <header>
-        { sidenav }
+        <Nav className="side-nav" id="side-nav" items={links} />
         <div className="navbar-fixed row">
           <nav className="teal lighten-1">
             <div className="nav-wrapper">
               <div className="col s12">
                 <Link to='/main' className="brand-logo">Votery</Link>
-                { links }
+                <Nav
+                  className="right hide-on-med-and-down"
+                  id="top-nav"
+                  items={links}
+                />
                 <a
                   className="button-collapse right hide-on-large"
                   href="#"
-                  data-activates="sideNav"
+                  data-activates="side-nav"
                 >
                   <i className="material-icons">menu</i>
                 </a>

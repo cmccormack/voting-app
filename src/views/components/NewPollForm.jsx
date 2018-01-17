42bb932ae559901df8e4dfbe22fa0c4899c050da
Christopher McCormack <mccormack.christopher@gmail.com>
2018-01-17 16:12:17 -0700

    Wrapped inputs in FromRow component and removed newchoice component

--- src/views/components/NewPollForm.jsx
+++ src/views/components/NewPollForm.jsx
@@ -6,35 +6,29 @@ import { Link } from 'react-router-dom'
 import {
   FormCard,
   FormSubmitButton,
-  FormInput
+  FormInput,
+  FormRow
 } from '../layout'
 
 
 const PollChoices = ({choices, ...props}) => (
   choices.map((choice, iter) => (
-    <FormInput 
-      choice={choice}
-      iter={iter}
-      key={`choice_${iter}`}
-      label={`Choice ${iter + 1}`}
-      name={`choice_${iter}`}
-      onChange={e => props.handleInputChange(e, e.target.value, iter)}
-      value={choice}
-      {...props}
-    />
+    <FormRow>
+      <FormInput 
+        choice={choice}
+        iter={iter}
+        key={`choice_${iter}`}
+        label={`Choice ${iter + 1}`}
+        name={`choice_${iter}`}
+        onChange={e => props.handleInputChange(e, e.target.value, iter)}
+        value={choice}
+        {...props}
+      />
+      <div className="btn">Delete</div>
+    </FormRow>
   ))
 )
 
-
-const NewChoice = props => {
-
-  return (
-    <FormInput
-      {...props}
-    />
-  )
-
-}
 
 class NewPollForm extends Component {
 
@@ -56,25 +50,29 @@ class NewPollForm extends Component {
       <FormCard {...props}>
         <form id="new_poll_form" onSubmit={this.props.handleSubmit}>
 
-          <FormInput
-            icon=""
-            label="Title (e.g. Favorite Superhero)"
-            name="title"
-            onChange={this.props.handleInputChange}
-            required
-            size="s12 m12 l8 offset-l2"
-            value={this.props.title}
-          />
+          <FormRow>
+            <FormInput
+              icon=""
+              label="Title (e.g. Favorite Superhero)"
+              name="title"
+              onChange={this.props.handleInputChange}
+              required
+              size="s12 m12 l8 offset-l2"
+              value={this.props.title}
+            />
+          </FormRow>
 
-          <FormInput
-            icon=""
-            label="Short Title (e.g. fav_superhero, optional)"
-            maxLength={16}
-            name="shortname"
-            onChange={this.props.handleInputChange}
-            size="s12 m12 l8 offset-l2"
-            value={this.props.shortname}
-          />
+          <FormRow>
+            <FormInput
+              icon=""
+              label="Short Title (e.g. fav_superhero, optional)"
+              maxLength={16}
+              name="shortname"
+              onChange={this.props.handleInputChange}
+              size="s12 m12 l8 offset-l2"
+              value={this.props.shortname}
+            />
+          </FormRow>
 
           <PollChoices 
             choices
@@ -84,23 +82,12 @@ class NewPollForm extends Component {
             {...this.props}
           />
 
-          <NewChoice
-            icon=""
-            label="Add New Choice"
-            maxLength={32}
-            name="newChoice"
-            onChange={this.props.handleInputChange}
-            size="s6 offset-s2"
-            value={this.props.newChoice}
-          />
-
-
-
-          <FormSubmitButton
-            onClick={this.props.handleSubmit}
-            position="right"
-          />
-
+          <FormRow>
+            <FormSubmitButton
+              onClick={this.props.handleSubmit}
+              position="right"
+            />
+          </FormRow>
 
         </form>
 

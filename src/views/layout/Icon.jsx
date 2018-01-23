dc924346e9b41f0b5cd48b62a1b3504b0b5d4703
Christopher McCormack <mccormack.christopher@gmail.com>
2018-01-22 19:21:02 -0700

    Initial Commit

---
+++ src/views/layout/Icon.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const IconStyled = styled.i`
+  font-size: ${props => props.fontSize};
+`
+
+const Icon = ({children, className, color, ...props}) => (
+
+  <IconStyled
+    className={`${className} ${color}`}
+    { ...props }
+  >
+    { children }
+  </IconStyled>
+
+)
+
+export default Icon
---
+++ src/views/layout/IconLink.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const IconLink = ({title='', href="#", Icon, ...props}) => (
+  <a 
+    title={ title }
+    href={ href }
+    onClick={ props.onClick }
+  >
+    { Icon }
+  </a>
+)
+
+export default IconLink

8a7ee20dea6109981f82b7f12ed1949c9f4b329c
Christopher McCormack <mccormack.christopher@gmail.com>
2018-02-04 22:19:51 -0700

    Working on pre-validation

--- models/poll.js
+++ models/poll.js
@@ -1,7 +1,7 @@
 
 module.exports = mongoose => {
   
-  const pollSchema = new mongoose.Schema({
+  const PollSchema = new mongoose.Schema({
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User'
@@ -35,6 +35,25 @@ module.exports = mongoose => {
     }]
   })
 
-  return mongoose.model('Poll', pollSchema, 'polls')
+  var Poll = mongoose.model('Poll', PollSchema, 'polls')
+  PollSchema.pre('validate', function(next) {
+    console.log(`presave - ${this}`)
+  })
+
+  
+  // PollSchema.pre('validate', function(next) {
+  //   console.log('****** Inside PollSchema pre ******')
+  //   console.log(`Poll presave: ${this}`)
+  //   Poll.find({createdBy: this.createdBy, title: this.title}, (err, docs) => {
+  //     if (docs) {
+  //       return next( new Error('Poll with that title already exists!') )
+  //     } else {
+  //       return next()
+  //     }
+  //   })
+  // })
+
+  Poll = mongoose.model('Poll', PollSchema, 'polls')
+  return Poll
 
 }

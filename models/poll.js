20fc5ee510929c221db4f3f24ffd85cba65f08fe
Christopher McCormack <mccormack.christopher@gmail.com>
2018-02-06 21:30:40 -0700

    working on pre save validation

--- models/poll.js
+++ models/poll.js
@@ -35,25 +35,24 @@ module.exports = mongoose => {
     }]
   })
 
-  var Poll = mongoose.model('Poll', PollSchema, 'polls')
-  PollSchema.pre('validate', function(next) {
-    console.log(`presave - ${this}`)
+  PollSchema.pre('save', function(next) {
+
+    Poll.find({})
+      .where('createdBy').equals(this.createdBy)
+      .or([ { title: this.title }, { shortName: this.shortName } ])
+      .exec((err, docs) => {
+        console.log(err)
+        console.log(docs)
+        if (docs.length > 0) {
+          return next( new Error('Poll with that title already exists!') )
+        } else {
+          return next()
+        }
+      })
   })
 
-  
-  // PollSchema.pre('validate', function(next) {
-  //   console.log('****** Inside PollSchema pre ******')
-  //   console.log(`Poll presave: ${this}`)
-  //   Poll.find({createdBy: this.createdBy, title: this.title}, (err, docs) => {
-  //     if (docs) {
-  //       return next( new Error('Poll with that title already exists!') )
-  //     } else {
-  //       return next()
-  //     }
-  //   })
-  // })
+  const Poll = mongoose.model('Poll', PollSchema, 'polls')
 
-  Poll = mongoose.model('Poll', PollSchema, 'polls')
   return Poll
 
 }

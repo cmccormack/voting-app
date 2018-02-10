65fcabd9a4fa25cac254b0e366146dbdfee223e9
Christopher McCormack <mccormack.christopher@gmail.com>
2018-02-10 16:57:07 -0700

    Updated

--- src/views/layout/Tabs.jsx
+++ src/views/layout/Tabs.jsx
@@ -26,18 +26,26 @@ class Tabs extends Component {
       <div className="row">
         <div className="col s12">
           <TabGroup className="tabs" indicatorColor="#00897b">
-            <Tab color="teal-text" disabled={false} disabledColor="teal-text text-lighten-3" target="#yourmomma" size="s3">Your momma</Tab>
-            <li className="tab col s3"><a href="#test1">Test 1</a></li>
-            <li className="tab col s3"><a className="active" href="#test2">Test 2</a></li>
-            <li className="tab col s3 disabled"><a href="#test3">Disabled Tab</a></li>
-            <li className="tab col s3"><a href="#test4">Test 4</a></li>
+            <Tab
+              color="teal-text"
+              disabled={false}
+              disabledColor="teal-text text-lighten-3"
+              target="#polls"
+              size="s3">
+                {'My Polls'}
+            </Tab>
+            <Tab
+              color="teal-text"
+              disabled={false}
+              disabledColor="teal-text text-lighten-3"
+              target="#settings"
+              size="s3">
+              {'Settings'}
+            </Tab>
           </TabGroup>
         </div>
-        <div id="yourmomma" className="col s12">Your momma</div>
-        <div id="test1" className="col s12">Test 1</div>
-        <div id="test2" className="col s12">Test 2</div>
-        <div id="test3" className="col s12">Test 3</div>
-        <div id="test4" className="col s12">Test 4</div>
+        <div id="polls" className="col s12">My Polls</div>
+        <div id="settings" className="col s12">My Settings</div>
       </div>
     )
   }

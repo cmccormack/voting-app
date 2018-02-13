b6bc43f1593c7a5303e4b4e7e83c6cef744d43e5
Christopher McCormack <mccormack.christopher@gmail.com>
2018-02-12 19:33:49 -0700

    Added action item components for groups of action items

--- src/views/layout/CollectionItem.jsx
+++ src/views/layout/CollectionItem.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-const CollectionItem = ({ title, props, action, header=false }) => (
+
+const ActionItemLink = styled.a`
+  margin-left: 10px;
+`
+
+const ActionItem = ({ icon, target, color }) => (
+  <ActionItemLink href={ target } className={`${color}`}>
+    <i className="material-icons">{ icon }</i>
+  </ActionItemLink>
+)
+
+
+const ActionItems = ({ actions, color }) => {
+
+  if (Array.isArray(actions)) {
+    return actions.map(action => (
+      <ActionItem
+        color={ color }
+        icon={ action.icon }
+        key={ action.target + action.icon }
+        target={ action.target }
+      />
+    ))
+  }
+
+  return (
+    <ActionItem
+      color={ color }
+      icon={ actions.icon }
+      target={ actions.target }
+    />
+  )
+}
+
+const ActionGroup = styled.div`
+  float: right;
+`
+
+const CollectionItem = ({ title, props, actions, header=false }) => (
   <li className={`collection-${header ? 'header' : 'item'}`}>
     <div>
       {title}
-      <a href={ action.target } className="secondary-content">
-        <i className="material-icons">{ action.icon }</i>
-      </a>
+      <ActionGroup>
+        <ActionItems
+          actions={actions}
+          color={'teal-text text-lighten-1'}
+        />
+      </ActionGroup>
     </div>
   </li>
 )

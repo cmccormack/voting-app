264840ab97f3b5cb938f1aeda46ed0200e559102
Christopher McCormack <mccormack.christopher@gmail.com>
2018-04-05 21:58:08 -0700

    Updated

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -3,7 +3,6 @@ import { randInt, randRangeInt } from './rand'
 export const getColor = (h, s, l) => `hsl(${h}, ${s}%, ${l}%)`
 
 export const getColorsIncrementHue = (length, inc=10, s, l, min=0, max=360) => (
-    Array(length).fill(randRangeInt(min, max))
-    .map((start,i) => getColor((start+(inc*i)%360+min), s, l))
-  )
+  Array(length).fill(randRangeInt(min, max))
+  .map((start,i) => getColor((start+(inc*i)%360+min), s, l))
 )

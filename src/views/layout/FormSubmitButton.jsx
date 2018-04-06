48081254dd7c2678c96e0c11519b5c8e5212f3a0
Christopher McCormack <mccormack.christopher@gmail.com>
2018-04-06 12:26:47 -0700

    Added additional properties

--- src/views/layout/FormSubmitButton.jsx
+++ src/views/layout/FormSubmitButton.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-const FormSubmitButton = ({size, onClick, icon='send', position='right'}) => (
+const FormSubmitButton = ({
+  buttonText='Submit',
+  icon='send',
+  onClick,
+  position='right',
+  size,
+}) => (
   <div className={`col ${ size }`}>
     <button
       className={`waves-effect waves-light btn ${position} hoverable`}
       onClick={onClick}
     >
       <i className="material-icons right">{icon}</i>
-      {'Submit'}
+      {buttonText}
     </button>
   </div>
 )

b2a7ca4daf50a87fc4087e924edb36c775d70309
Christopher McCormack <mccormack.christopher@gmail.com>
2018-04-08 10:14:04 -0700

    Addeded comopnent

--- src/views/components/index.js
+++ src/views/components/index.js
@@ -2,6 +2,7 @@ import RegisterForm from './RegisterForm'
 import LoginForm from './LoginForm'
 import NewPollForm from './NewPollForm'
 import UserAccountForm from './UserAccountForm'
+import UserPollsForm from './UserPollsForm'
 import ViewPollForm from './ViewPollForm'
 
 export {
@@ -9,5 +10,6 @@ export {
   LoginForm,
   NewPollForm,
   UserAccountForm,
-  ViewPollForm
+  UserPollsForm,
+  ViewPollForm,
 }

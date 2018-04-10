097fc785899895574838e333429a70019cf807b2
Christopher McCormack <mccormack.christopher@gmail.com>
2018-04-09 21:18:14 -0700

    Initial Commit

---
+++ src/views/utils/LoaderWithTimeout.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class APILoaderWithTimeout extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      renderComponent: this.props.renderLoading
+    }
+    this.timeoutID = 0
+  }
+
+  componentDidMount() {
+    const { renderFailed, timeout, } = this.props
+
+    this.timeoutID = setTimeout(() => {
+      console.log('Loading timed out...')
+      this.setState({
+        renderComponent: renderFailed
+      })
+    }, timeout)
+
+  }
+
+  render() {
+    console.log(this.props.loaded)
+
+    this.props.loaded && clearInterval(this.timeoutID)
+
+    return this.props.loaded
+      ? this.props.renderSuccess
+      : this.state.renderComponent
+  }
+
+}
+
+export default APILoaderWithTimeout

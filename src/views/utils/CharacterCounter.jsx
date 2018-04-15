b51d6f67c2c02fcbd268ccbc51c8c7726dfc6b21
Christopher McCormack <mccormack.christopher@gmail.com>
2018-04-15 08:10:33 -0700

    Added helper function for position

--- src/views/utils/CharacterCounter.jsx
+++ src/views/utils/CharacterCounter.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const getPositionOrDefault = value => value == undefined ? 'unset' : value
 
 const CharacterCounterStyled = styled.span`
   color: ${props => props.color};
   display: ${props => props.isVisible ? 'block' : 'none'};
   position: absolute;
-  top: ${props => props.top || 'unset'};
-  /* was -10 */
-  right: ${props => props.right || 'unset'};
-  bottom: ${props => props.bottom || 'unset'};
-  left: ${props => props.left || 'unset'};
-  /* was 4 */
+  top: ${props => getPositionOrDefault(props.top)};
+  right: ${props => getPositionOrDefault(props.right)};
+  bottom: ${props => getPositionOrDefault(props.bottom)};
+  left: ${props => getPositionOrDefault(props.left)};
   line-height: ${props => props.lineHeight || 'normal'};
 `
 
 const CharacterCounter = ({ count, max, ...props }) => (
   <CharacterCounterStyled
-    color={count > max * .8 ? 'red' : count > max * .5 ? 'orange' : 'green'}
-    isVisible={count > max * .5}
+    color={ count > max * .8 ? 'red' : count > max * .5 ? 'orange' : 'green' }
+    isVisible={ count > max * .5 }
     { ...props }
   >
-    {console.log(count)}
     { count }
   </CharacterCounterStyled>
 )

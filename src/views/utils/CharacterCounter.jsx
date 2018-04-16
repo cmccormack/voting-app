409e3e084e407f00e93ad544224df8e7acb97b18
Christopher McCormack <mccormack.christopher@gmail.com>
2018-04-15 18:19:26 -0700

    Added default values and some cleanup

--- src/views/utils/CharacterCounter.jsx
+++ src/views/utils/CharacterCounter.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 
-const getPositionOrDefault = value => value == undefined ? 'unset' : value
-
 const CharacterCounterStyled = styled.span`
-  color: ${props => props.color};
-  display: ${props => props.isVisible ? 'block' : 'none'};
+  color: ${props => props.color || props.theme.color};
+  display: ${props => (props.isVisible || props.theme.isVisible) 
+    ? 'block' 
+    : 'none'
+  };
   position: absolute;
-  top: ${props => getPositionOrDefault(props.top)};
-  right: ${props => getPositionOrDefault(props.right)};
-  bottom: ${props => getPositionOrDefault(props.bottom)};
-  left: ${props => getPositionOrDefault(props.left)};
-  line-height: ${props => props.lineHeight || 'normal'};
+  top: ${props => props.top || props.theme.top};
+  right: ${props => props.right || props.theme.right};
+  bottom: ${props => props.bottom || props.theme.bottom};
+  left: ${props => props.left || props.theme.left};
+  line-height: ${props => props.lineHeight || props.theme.lineHeight};
 `
 
-const CharacterCounter = ({ count, max, ...props }) => (
-  <CharacterCounterStyled
-    color={ count > max * .8 ? 'red' : count > max * .5 ? 'orange' : 'green' }
-    isVisible={ count > max * .5 }
-    { ...props }
-  >
-    { count }
-  </CharacterCounterStyled>
-)
+class CharacterCounter extends Component {
+
+  render() {
+
+    const { count, max, ...props } = this.props
+
+    return(
+      <CharacterCounterStyled
+        color={ count > max * .8 ? 'red' : count > max * .5 ? 'orange' : 'green' }
+        isVisible={ count > max * .5 }
+        { ...props }
+      >
+        { count }
+      </CharacterCounterStyled>
+
+    )
+
+  }
+}
+
+CharacterCounter.defaultProps = {
+  theme: {
+    top: 0,
+    right: 'unset',
+    bottom: 0,
+    left: 'unset',
+    color: 'initial',
+    lineHeight: 'normal',
+    isVisible: false
+  }
+}
 
 export default CharacterCounter

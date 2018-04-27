83ee876ec665871ff962a9f071713122fb0ea1d3
Christopher McCormack <mccormack.christopher@gmail.com>
2018-04-27 12:51:27 -0700

    Added proptypes and classNames

--- src/views/layout/FormRow.jsx
+++ src/views/layout/FormRow.jsx
@@ -1,14 +1,27 @@
-import React, { Component, } from 'react'
+import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import classNames from "classnames";
 
 const FormRowStyled = styled.div`
   position: relative;
 `
 
 const FormRow = ({ align="", children, className="", }) => (
-  <FormRowStyled className={`row ${align} ${className}`.replace(/\s+/, ' ')}>
+  <FormRowStyled className={ classNames('row', align, className,) }>
     { children }
   </FormRowStyled>
 )
+
+FormRow.propTypes = {
+  align: PropTypes.string,
+  children: PropTypes.any,
+  className: PropTypes.string,
+}
+
+FormRow.defaultProps = {
+  align: 'left',
+  className: '',
+}
 
 export default FormRow
